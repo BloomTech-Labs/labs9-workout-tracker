35d88d7ebf78db3542d64e94034b6a3984dc55c0
Chris Cedeno <chrisrey95@gmail.com>
2019-01-08 16:43:55 -0500

    created user endpoint to retrive specific user data
    Co-authored-by: Joseph Stanfield <jlstan06@hotmail.com>

--- server/config/configureRoutes.js
+++ server/config/configureRoutes.js
@@ -1,5 +1,7 @@
 const defaultRoute = require('../routes/defaultRoute');
+const userRoute = require('../routes/usersRoute');
 
 module.exports = server => {
     server.use('/alive', defaultRoute);
+    server.use('/api/user', userRoute);
 }
--- server/knexfile.js
+++ server/knexfile.js
@@ -5,7 +5,7 @@ module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './dev.sqlite3'
+      filename: './database/dev.sqlite3'
     },
     useNullAsDefault: true,
     migrations: {
