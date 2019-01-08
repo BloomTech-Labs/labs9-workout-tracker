0c19d0cdb49c0a14ae1d3c27afd3e238f70b3610
Chris Cedeno <chrisrey95@gmail.com>
2019-01-08 18:22:42 -0500

    updated gitignore, knexfile for production on postgress, switch to async function for userid route
    Co-authored-by: Joseph Stanfield <jlstan06@hotmail.com>

--- server/knexfile.js
+++ server/knexfile.js
@@ -1,5 +1,15 @@
 // Update with your config settings.
 
+
+const localPgConnection = {
+  host: 'localhost',
+  database: 'keep',
+  user:     'username',
+  password: 'password'
+}
+
+const dbConnection = process.env.DATABASE_URL || localPgConnection;
+
 module.exports = {
 
   development: {
@@ -8,12 +18,22 @@ module.exports = {
       filename: './database/dev.sqlite3'
     },
     useNullAsDefault: true,
-    migrations: {
-      directory: './database/migrations'
+    migrations: { directory: './database/migrations'},
+    seeds: { directory: './database/seeds'},
+  },
+
+  production: {
+    client: 'pg',
+    connection: dbConnection,
+    pool: {
+      min: 2,
+      max: 10
     },
-    seeds: {
-      directory: './database/seeds'
-    }
-  }
+    migrations: { directory: './database/migrations'},
+    seeds: { directory: './database/seeds'},
+    connection: {
+      filename: './database/dev.sqlite3'
+    },
+  },
 
 };
