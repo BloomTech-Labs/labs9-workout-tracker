7aeb31a9343255d613ad733091e4ff63a55fb2b4
Chris Cedeno <chrisrey95@gmail.com>
2019-01-08 23:26:33 -0500

    updated Procfile naming and knexfile using dotenv

--- server/database/dbConfig.js
+++ server/database/dbConfig.js
@@ -1,4 +1,6 @@
+require('dotenv').config();
 const dbEngine = process.env.DB_ENGINE || 'development'
+console.log('dbEngine', dbEngine);
 const knex = require('knex');
 const knexConfig = require('../knexfile.js');
 
