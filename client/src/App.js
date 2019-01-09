15f3dd0744284306ba5e60c0587fad4415cef526
Lambda_School_Loaner_46 <lambda_school_loaner_46@Lambda-School-Loaner-46s-MacBook-Pro.local>
2019-01-09 15:57:34 -0500

    Completed data model to align backend and frontend with how the data will be received. Set up basic skeleton for settings page. Set up Calendar component.
    Co-authored-by: Elvis Ibarra <ielvisd@gmail.com>
    Co-authored-by: Joseph Stanfield <jlstan06@hotmail.com>

--- client/src/App.js
+++ client/src/App.js
@@ -16,7 +16,7 @@ const url = `https://labs9-workout-tracker.herokuapp.com/api`;
 const StyledApp = styled.div`
   text-align: center;
   width: 100%;
-  max-width: 880px;
+  max-width: 1200px;
   margin: 0 auto;
   border: 1px solid black;
   font-size: 62.5%;
@@ -27,7 +27,88 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      user: {}
+      users: [
+        {
+          id: 1,
+          name: "Elvis",
+          email: "jlstan06@yahoo.com",
+          phone: "859-598-8979",
+          receives_text: true,
+          recieves_email: false,
+          created_at: "", //timestamp obj
+          updated_at: '',
+          metrics: [
+            {
+            id: 1,
+            created_at: "", //timestamp obj
+            updated_at: '',
+            user_id: 1,
+            dateLogged: "",
+            weight: 2,
+            hips: 2,
+            waist: 2,
+            arm_right: 2,
+            arm_left: 2,
+            leg_left: 2,
+            leg_right: 2
+          }
+          ],
+          workouts: [
+            {
+              id:1,
+              title:'',
+              user_id:1,
+              category_id:1,
+              exercises: [
+                {
+                  id: 1,
+                  workout_id:1,
+                  name: "",
+                  sets: 2, //string or number
+                  reps: 2,
+                  weight:2,
+                }
+              ],
+              category: 
+                {
+                  id: 1,
+                  user_id:1,
+                  name: "",
+                }
+            }
+
+          ],
+          schedule_workouts: [
+            {
+              id:1,
+              date: '',
+              completed: false,
+              percentage: 0,
+              title:'',
+              user_id:1,
+              category_id:1,
+              schedule_exercises: [
+                {
+                  id: 1,
+                  schedule_workout_id:1,
+                  name: "",
+                  sets: 2, //string or number
+                  reps: 2,
+                  weight:2,
+                  completed:false
+                }
+              ],
+              category: 
+                {
+                  id: 1,
+                  user_id:1,
+                  name: "",
+                }
+            }
+
+          ],
+        }, 
+      ]
     };
   }
 
