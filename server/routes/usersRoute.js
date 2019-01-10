0c2fff65ade24d328afdcfc86e77d4bb41407a50
CookieMonsta89 <jlstan06@gmail.com>
2019-01-10 14:01:58 -0500

    Added endpoints for metric to GET, POST and DELETE for Users
    Co-authored-by: William DiFulvio <w.difulvio523@gmail.com>
    Co-authored-by: Elvis Ibarra <ielvisd@gmail.com>
    Co-authored-by: Christopher Cedeno <chrisrey95@gmail.com>

--- server/routes/usersRoute.js
+++ server/routes/usersRoute.js
@@ -1,54 +1,147 @@
-const express = require('express');
-const db = require('../database/dbConfig')
+const express = require("express");
+const db = require("../database/dbConfig");
 const router = express.Router();
+const knex = require("../database/dbConfig.js");
 
-router.get('/info/:id', async (req, res) => {
-    try {
-        const userInfo = await db('users').where('id', '=', req.params.id);
-        if(userInfo.length === 0) {
-            res.status(404).json({message: "That user doesnt exist"});
-        }
-        userId = userInfo[0].id;
-        
-        const metrics = await db('metrics').where('user_id', '=', userId)
-
-        const workouts = await db('workouts').where('user_id', '=', userId)
-        let workoutsArray = [];
-        for (const workout of workouts) {
-            const exercises = await db('exercises').where('workout_id', '=', workout.id)
-            const category = await db('category').where('id', '=', workout.category_id)
-            const workObj = {
-                ...workout,
-                exercises: [...exercises],
-                category: category[0]
-            }
-            workoutsArray.push(workObj)
-        }
-
-        const sWorkouts = await db('schedule_workouts').where('user_id', '=', userId)
-        let sWorkoutsArray = [];
-        for (const workout of sWorkouts) {
-            const exercises = await db('schedule_exercises').where('schedule_workout_id', '=', workout.id)
-            const category = await db('category').where('id', '=', workout.category_id)
-            const workObj = {
-                ...workout,
-                exercises: [...exercises],
-                category: category[0]
-            }
-            sWorkoutsArray.push(workObj)
-        }
-        
-        userObj = {
-            ...userInfo[0],
-            metrics: [...metrics],
-            workouts: workoutsArray,
-            scheduleWorkouts: sWorkoutsArray
-        }
-        res.status(200).json(userObj);
-    } catch(error) {
-        res.status(500).json({error, "Well this is embarrassing": "Something went wrong"})
+router.get("/info/:id", async (req, res) => {
+  try {
+    const userInfo = await db("users").where("id", "=", req.params.id);
+    if (userInfo.length === 0) {
+      res.status(404).json({ message: "That user doesnt exist" });
     }
-})
+    userId = userInfo[0].id;
 
+    const metrics = await db("metrics").where("user_id", "=", userId);
+
+    const workouts = await db("workouts").where("user_id", "=", userId);
+    let workoutsArray = [];
+    for (const workout of workouts) {
+      const exercises = await db("exercises").where(
+        "workout_id",
+        "=",
+        workout.id
+      );
+      const category = await db("category").where(
+        "id",
+        "=",
+        workout.category_id
+      );
+      const workObj = {
+        ...workout,
+        exercises: [...exercises],
+        category: category[0]
+      };
+      workoutsArray.push(workObj);
+    }
+
+    const sWorkouts = await db("schedule_workouts").where(
+      "user_id",
+      "=",
+      userId
+    );
+    let sWorkoutsArray = [];
+    for (const workout of sWorkouts) {
+      const exercises = await db("schedule_exercises").where(
+        "schedule_workout_id",
+        "=",
+        workout.id
+      );
+      const category = await db("category").where(
+        "id",
+        "=",
+        workout.category_id
+      );
+      const workObj = {
+        ...workout,
+        exercises: [...exercises],
+        category: category[0]
+      };
+      sWorkoutsArray.push(workObj);
+    }
+
+    userObj = {
+      ...userInfo[0],
+      metrics: [...metrics],
+      workouts: workoutsArray,
+      scheduleWorkouts: sWorkoutsArray
+    };
+    res.status(200).json(userObj);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error, "Well this is embarrassing": "Something went wrong" });
+  }
+});
+
+router.post("/", async (req, res) => {
+  const UserData = req.body;
+  console.log(req.body);
+  if (!UserData.name || !UserData.email || !UserData.phone) {
+    res
+      .status(422)
+      .json({ errorMessage: "Please provide a name, email, and phone number" });
+  } else if (!db("users").where("id", "=", req.params.id)) {
+    res.status(405).json({ errorMessage: "Duplicate User Titles Not Allowed" });
+  } else {
+    try {
+      const newUser = await db("users").insert(UserData);
+      res.status(201).json(newUser);
+    } catch (error) {
+      res.status(500).json({
+        error:
+          "There was an error while saving the user to the database. The error is ",
+        error
+      });
+    }
+  }
+});
+
+router.delete("/delete/:id", async (req, res) => {
+  const userId = req.params.id;
+
+  try {
+    const deleteduserCount = await db("users")
+      .where("id", "=", req.params.id)
+      .del(userId);
+    {
+      deleteduserCount === 0
+        ? res
+            .status(404)
+            .json({ message: "The user with the specified ID does not exist." })
+        : res.status(200).json({ deleteduserCount });
+    }
+  } catch (error) {
+    console.log("the req.params.id is... ", req.params.id);
+    console.log("the error is... ", error);
+
+    res.status(500).json("the error is... ", error);
+  }
+});
+
+router.put("/edit/:userId", async (req, res) => {
+  const changes = req.body;
+  const { userId } = req.params;
+
+  if (!changes.name || !changes.email || !changes.phone) {
+    res
+      .status(400)
+      .json({ errorMessage: "Please provide a name/email/phone for the user" });
+  }
+
+  try {
+    const updateduserCount = await db("users").update(userId, changes);
+    {
+      updateduserCount === 0
+        ? res
+            .status(404)
+            .json({ message: "The user with the specified ID does not exist." })
+        : res.status(200).json({ updateduserCount });
+    }
+  } catch (error) {
+    console.log("the req.params.userId is... ", req.params.userId);
+    console.log("the error is... ", error);
+    res.status(500).json(error);
+  }
+});
 
 module.exports = router;
