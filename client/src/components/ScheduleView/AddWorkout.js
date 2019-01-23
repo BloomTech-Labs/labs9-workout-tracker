6bdaf0fc30877f470effc00a8bd3640df6da92f6
William DiFulvio <w.difulvio523@gmail.com>
2019-01-22 22:23:57 -0500

    began functionality for scheduling a workout and adding to the calendar

--- client/src/components/ScheduleView/AddWorkout.js
+++ client/src/components/ScheduleView/AddWorkout.js
@@ -3,44 +3,46 @@ import styled from "styled-components";
 import axios from "axios";
 
 const AddWorkout = props => {
-  const key = window.localStorage.getItem('login_token');
-  const reqUrl = 'https://fitmetrix.herokuapp.com/api/category/user';
+  const key = window.localStorage.getItem("login_token");
+  const reqUrl = "https://fitmetrix.herokuapp.com/api/category/user";
 
-  const initialCategoryValue = [{ id: null, name: ' --- Select a Category --- ' }];
+  const initialCategoryValue = [
+    { id: null, name: " --- Select a Category --- " }
+  ];
   const [categories, setCategory] = useState(initialCategoryValue);
-  const [categoryID, setCategoryID] = useState(null)
+  const [categoryID, setCategoryID] = useState(null);
 
-//create category component variable to put in the dropdown.
-let categoryComponent = null;
+  //create category component variable to put in the dropdown.
+  let categoryComponent = null;
 
-// useEffect to get Categories from the backend
-useEffect(() => {
-  axios.get(reqUrl, { headers: { Authorization: key } }).then(
-    result => (
-      console.log('the result is: ', result.data),
-      result.data.map(res => {
-        console.log('the res is: ', res);
-        initialCategoryValue.push(res);
-        console.log('initialCategoryValue:', initialCategoryValue);
-        return setCategory(initialCategoryValue);
-      })
-    )
-  );
-}, []);
+  // useEffect to get Categories from the backend
+  useEffect(() => {
+    axios.get(reqUrl, { headers: { Authorization: key } }).then(
+      result => (
+        console.log("the result is: ", result.data),
+        result.data.map(res => {
+          console.log("the res is: ", res);
+          initialCategoryValue.push(res);
+          console.log("initialCategoryValue:", initialCategoryValue);
+          return setCategory(initialCategoryValue);
+        })
+      )
+    );
+  }, []);
 
   // method to handle selecting a category from dropdown
   const categorySelectionHandler = event => {
     let value = event.target.value;
-    console.log('value', value)
-    console.log('categories', categories)
+    console.log("value", value);
+    console.log("categories", categories);
     const categoryIndex = categories.filter(category => {
       return category.id === Number(value);
     });
-    setCategoryID(categoryIndex[0].id)
+    setCategoryID(categoryIndex[0].id);
   };
 
-   //Puts the categories into a component
-   categoryComponent = (
+  //Puts the categories into a component
+  categoryComponent = (
     <select
       onChange={e => {
         categorySelectionHandler(e);
@@ -55,22 +57,41 @@ useEffect(() => {
     </select>
   );
 
+  //handler to schedule the workout and add it to Sworkout Database
+  const scheduleWorkoutHandler = (e, workout) => {
+    e.preventDefault();
+    //incoming workout object
+    console.log("schedworkouthandler wkt:", workout);
+    // add to scheduled workout array
+
+    //should re-render on calender as a scheduled workout
+
+  };
+
   return (
     <AddWorkoutStyle>
       <form>
         <div>
-        <div>{categoryComponent}</div>
-           {props.workouts &&
-            props.workouts.map( workout => {
-              console.log('map workout:', workout)
-              console.log('map categoryID:', categoryID)
+          <div>{categoryComponent}</div>
+          {props.workouts &&
+            props.workouts.map(workout => {
+              console.log("map workout:", workout);
+              console.log("map categoryID:", categoryID);
               if (workout.id === categoryID) {
                 return (
-                  <div>{workout.title}</div>
-                )
+                  <div>
+                    <div>{workout.title}</div>
+                    <button
+                      onClick={ (e) => {
+                        scheduleWorkoutHandler(e, workout)
+                       } }
+                    >
+                      Schedule
+                    </button>
+                  </div>
+                );
               }
-            })
-            } 
+            })}
         </div>
       </form>
     </AddWorkoutStyle>
