0cac71c0709713ca13daaac2fd4e931d18bf8c46
Elvis Ibarra <ielvisd@gmail.com>
2019-01-29 10:26:29 -0800

    Delete a workout works with new state management
    Co-authored-by: Matt Wright <matthewbwright@gmail.com>

--- client/src/index.js
+++ client/src/index.js
@@ -1,59 +1,55 @@
-import React, { useReducer, useEffect, createContext } from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import { BrowserRouter as Router } from "react-router-dom";
-import firebase from "firebase";
+import React, { useReducer, useEffect, createContext } from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import { BrowserRouter as Router } from 'react-router-dom';
+import firebase from 'firebase';
 
 export const Store = createContext({ state: {}, dispatch: () => {} });
 
 const initialState = {
   category: [],
-  email: "",
+  email: '',
   id: null,
-  name: "",
+  name: '',
   phone: null,
   premium: null,
   recieves_email: null,
   recieves_text: null,
   editWorkout: null,
   scheduleWorkouts: [],
-  uid: "",
+  uid: '',
   workouts: [],
   editMetric: null,
   showMetricForm: false,
-  selectedCategory: "default",
-<<<<<<< HEAD
-  selectedWorkoutCategory: "all",
-=======
-  selectedWorkoutCategory: "default"
->>>>>>> 39545222daa7755c675fcf10b1939272cec051be
-  selectedDate:new Date(),
+  selectedCategory: 'default',
+  selectedWorkoutCategory: 'all',
+  selectedDate: new Date()
 };
 
 const reducer = (state, action) => {
   switch (action.type) {
-    case "USER_MODEL":
+    case 'USER_MODEL':
       return { ...state, ...action.payload };
-    case "UPDATE_METRICS":
+    case 'UPDATE_METRICS':
       return { ...state, metrics: [...action.payload] };
-    case "UPDATE_SCHEDULE_WORKOUTS":
+    case 'UPDATE_SCHEDULE_WORKOUTS':
       return { ...state, scheduleWorkouts: [...action.payload] };
-    case "EDIT_WORKOUT":
+    case 'EDIT_WORKOUT':
       return { ...state, editWorkout: { ...action.payload } };
-    case "RESET_EDIT_WORKOUT":
+    case 'RESET_EDIT_WORKOUT':
       return { ...state, editWorkout: null };
-    case "UPDATE_WORKOUTS":
+    case 'UPDATE_WORKOUTS':
       return { ...state, workouts: [...action.payload] };
-    case "EDIT_METRIC":
+    case 'EDIT_METRIC':
       return { ...state, editMetric: { ...action.payload } };
-    case "RESET_EDIT_METRIC":
+    case 'RESET_EDIT_METRIC':
       return { ...state, editMetric: null };
-    case "SHOW_METRIC_FORM":
+    case 'SHOW_METRIC_FORM':
       return { ...state, showMetricForm: !state.showMetricForm };
-    case "UPDATE_SELECTED_CATEGORY":
+    case 'UPDATE_SELECTED_CATEGORY':
       return { ...state, selectedCategory: action.payload };
-    case "UPDATE_SELECTED_WORKOUTS_CATEGORY":
+    case 'UPDATE_SELECTED_WORKOUTS_CATEGORY':
       return { ...state, selectedWorkoutCategory: action.payload };
     default:
       // A reducer must always return a valid state.
@@ -65,12 +61,12 @@ const reducer = (state, action) => {
 const AppContainer = () => {
   useEffect(() => {
     var config = {
-      apiKey: "AIzaSyAQRB_UBjCXzDmxluLuDiM-VUjEoi9HjnQ",
-      authDomain: "fitmetrix-57cce.firebaseapp.com",
-      databaseURL: "https://fitmetrix-57cce.firebaseio.com",
-      projectId: "fitmetrix-57cce",
-      storageBucket: "fitmetrix-57cce.appspot.com",
-      messagingSenderId: "771224902694"
+      apiKey: 'AIzaSyAQRB_UBjCXzDmxluLuDiM-VUjEoi9HjnQ',
+      authDomain: 'fitmetrix-57cce.firebaseapp.com',
+      databaseURL: 'https://fitmetrix-57cce.firebaseio.com',
+      projectId: 'fitmetrix-57cce',
+      storageBucket: 'fitmetrix-57cce.appspot.com',
+      messagingSenderId: '771224902694'
     };
     firebase.initializeApp(config);
   }, []);
@@ -86,4 +82,4 @@ const AppContainer = () => {
   );
 };
 
-ReactDOM.render(<AppContainer />, document.getElementById("root"));
+ReactDOM.render(<AppContainer />, document.getElementById('root'));
