22a4c254c1cf67869bc352ce6283d6d6a5533ecc
Chris Cedeno <chrisrey95@gmail.com>
2019-01-31 00:55:55 -0500

    progress details fixed for arms and legs

--- client/src/components/ProgressView/ProgressGraph/Details.js
+++ client/src/components/ProgressView/ProgressGraph/Details.js
@@ -1,5 +1,5 @@
 import React, { useContext, useState, useEffect } from 'react';
-import styled from 'styled-components'
+import styled, {css} from 'styled-components'
 import { Store } from '../../../index';
 import { Radar } from 'react-chartjs-2';
 
@@ -33,7 +33,7 @@ const Details = () => {
         if (i > 1 || i < 8){
           const value = metrics[latest.index][keys[i]] - metrics[earliest.index][keys[i]];
 
-          radarData.push(value > 0 ? value : 0)
+          radarData.push(value > 0 ? Math.round(value *100)/100 : 0)
         }
       }
 
@@ -44,9 +44,9 @@ const Details = () => {
       labels: ["Weight", "Hips", "Waist", "Arm Left", "Arm Right", "Leg Left", "Leg Right"],
       datasets: [{
         label: 'Progress',
-        borderColor: 'rgba(253, 143, 37, 1)',
-        backgroundColor: 'rgba(253, 143, 37, 0.4)',
-        pointBackgroundColor: 'rgba(253, 143, 37, 1)',
+        borderColor: 'rgba(43, 58, 66, 1)',
+        backgroundColor: 'rgba(43, 58, 66, 0.4)',
+        pointBackgroundColor: 'rgba(43, 58, 66, 1)',
         // data: [10.62, 20.57, 37, 20.01, 10.5, 20.01, 10.5]
         data: getData()
     }]}
@@ -68,6 +68,67 @@ const Details = () => {
           }
         });
         
+        if (state.graphType === "legs") {
+          const leftPro = metrics[latest.index]["leg_left"] - metrics[earliest.index]["leg_left"];
+          const rightPro = metrics[latest.index]["leg_right"] - metrics[earliest.index]["leg_right"];
+          return (
+            <DetailsContainer>
+                <h2>{state.graphType}</h2>
+                <Row>
+                  <h3>Left Progress:</h3>
+                  <span>{Math.round(leftPro * 100) /100}in</span>
+                </Row>
+                <Row>
+                  <h3>Left Current: </h3>
+                  <span>{metrics[latest.index]["leg_left"]}in</span>
+                </Row>
+                <Row right>
+                  <h3>Right Progress:</h3>
+                  <span>{Math.round(rightPro * 100) /100}in</span>
+                </Row>
+                <Row right>
+                  <h3>Right Current: </h3>
+                  <span>{metrics[latest.index]["leg_right"]}in</span>
+                </Row>
+                <RadarContianer>
+                  <Radar data={radarData}/>
+                </RadarContianer>
+            </DetailsContainer>
+          );
+        }
+
+        if (state.graphType === "arms") {
+          const leftPro = metrics[latest.index]["arm_left"] - metrics[earliest.index]["arm_left"];
+          const rightPro = metrics[latest.index]["arm_right"] - metrics[earliest.index]["arm_right"];
+          return (
+            <DetailsContainer>
+                <h2>{state.graphType}</h2>
+                <Row>
+                  <h3>Left Progress:</h3>
+                  <span>{Math.round(leftPro * 100) /100}in</span>
+                </Row>
+                <Row>
+                  <h3>Left Current: </h3>
+                  <span>{metrics[latest.index]["arm_left"]}in</span>
+                </Row>
+                <Row right>
+                  <h3>Right Progress:</h3>
+                  <span>{Math.round(rightPro * 100) /100}in</span>
+                </Row>
+                <Row right>
+                  <h3>Right Current: </h3>
+                  <span>{metrics[latest.index]["arm_right"]}in</span>
+                </Row>
+                <RadarContianer>
+                  <Radar data={radarData}/>
+                </RadarContianer>
+            </DetailsContainer>
+          );
+        }
+
+
+
+
         const progress = metrics[latest.index][type] - metrics[earliest.index][type];
 
         return (
@@ -75,11 +136,11 @@ const Details = () => {
                 <h2>{state.graphType}</h2>
                 <Row>
                   <h3>Progress:</h3>
-                  <span>{Math.round(progress * 100) /100}</span>
+                  <span>{Math.round(progress * 100) /100}{state.graphType === "weight" ? "lbs" : "in"}</span>
                 </Row>
                 <Row>
                   <h3>Current: </h3>
-                  <span>{metrics[latest.index][type]}</span>
+                  <span>{metrics[latest.index][type]}{state.graphType === "weight" ? "lbs" : "in"}</span>
                 </Row>
                 <RadarContianer>
                   <Radar data={radarData}/>
@@ -99,22 +160,28 @@ const RadarContianer = styled.div`
     width: 375px;
     position: absolute;
     right: -90px;
-    top: 137px;
+    top: 200px;
 `;
 
 const Row = styled.div`
   display: flex;
-  align-items: space-between;
+  align-items: center;
+  justify-content: space-between;
   width: 100%;
+  margin-bottom: 10px;
   h3 {
-    font-size: 18px;
+    font-size: 15px;
     font-family: 600;
     margin: 0;
   }
   span{
-    font-size: 17px;
-    font-family: 600;
+    font-size: 18px;
+    font-weight: 600;
     margin-left: 20px;
+    color: ${props => props.theme.accent};
+    ${props => props.right && css`
+      color: ${props => props.theme.primaryLight};
+    `}
   }
 `;
 
@@ -127,18 +194,12 @@ const DetailsContainer = styled.div`
   width: 20%;
   position: relative;
   /* background-color: ${props => props.theme.primaryDark}; */
-  color: ${props => props.theme.primaryDark};
   padding: 20px 10px;
 
   h2 {
       font-size: 20px;
       font-weight: 600;
       text-transform: uppercase;
-  }
-
-  span {
-    font-size: 14px;
-    font-weight: 500;
-    margin-bottom: 10px;
+      color: ${props => props.theme.primaryDark};
   }
 `;
