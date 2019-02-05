8d39e6d8b5df4845ed3b1c01defdd98224402d77
William DiFulvio <w.difulvio523@gmail.com>
2019-02-05 13:24:21 -0500

    Schedule workout changes
    Functionality for add workout complete. BugFix completed. Styling completed. Setting style completed.
    
    Co-authored-by: Joseph Stanfield <jlstan06@hotmail.com>

--- client/src/components/SettingsView/BillingView.js
+++ client/src/components/SettingsView/BillingView.js
@@ -41,26 +41,28 @@ const StripeButton = () => {
       image="https://images.pexels.com/photos/1229356/pexels-photo-1229356.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" //Pop-in header image
       billingAddress={false} //asks for less info. Delete to ask for billing address
     >
-      <StripeStyle>Upgrade to Premium</StripeStyle>
+      <StripeStyle>Upgrade Premium</StripeStyle>
     </StyleCheckout>
   );
 };
 export default StripeButton;
 
 const StripeStyle = styled.button`
-  background-color: ${props => props.theme.accent};
-  font-size: 1.4rem;
-  position: relative;
-  display: block;
-  height: 40px;
-  color: white;
-  font-weight: bold;
-  box-shadow: rgba(255, 255, 255, 0.25) 0px 1px 0px inset;
-  border-radius: 5px;
-  text-align: center;
-  width: 180px;
-  padding: 5px 50px;
-  justify-content:center;
+width:86%;
+text-transform: uppercase;
+letter-spacing: 1px;
+display: inline-block;
+height: 50px;
+line-height: 50px;
+padding: 0 50px;
+font-size: 12px;
+font-weight: 700;
+transition: box-shadow .2s ease,border .2s ease,-webkit-box-shadow .2s ease;
+border-radius: 100px;
+outline: none;
+background: #FD8F25;
+border: none;
+color: #FFF;
 `;
 
 const StyleCheckout = styled(StripeCheckout)`
